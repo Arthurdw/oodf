b7346a5eff1e668e9b2a242cdb0fe047dc4d9e1f
Arthurdw <mail@arthurdw.com>
2022-01-10 13:41:16 +0100

    :bookmark: Dev version 1

--- libraries/python/oodf/__lib__.py
+++ libraries/python/oodf/__lib__.py
@@ -1,12 +1,12 @@
 # Project is under an MIT-style license that can be found in the LICENSE file at the monorepo
 # Monorepo: https://github.com/Arthurdw/oodf/libraries/python
 
-__version_major__ = "0"
-__version_minor__ = "0"
-__version_patch__ = "1"
+__version_major__ = 0
+__version_minor__ = 0
+__version_patch__ = 1
 __version_extra__ = "dev"
-__version_ident__ = 0
-__version__ = ".".join([__version_major__, __version_minor__, __version_patch__]) + \
+__version_ident__ = 1
+__version__ = ".".join(map(str, [__version_major__, __version_minor__, __version_patch__])) + \
               (f".{__version_extra__}{__version_ident__}" if __version_extra__ else "")
 
 __package__ = "oodf"
--- upload-python.sh
+++ upload-python.sh
@@ -1,2 +1,4 @@
 python libraries/python/setup.py sdist
+rm -rf libraries/python/oodf.egg-info
 twine upload libraries/python/dist/*
+rm -rf libraries/python/dist
